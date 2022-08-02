4886067876986ff822f06629654b6d5c7c5f7ad0
Jacob Anderson <andersonjwan@gmail.com>
2022-08-02 11:41:13 -0400

    feat: Implement custom interpreter error

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,17 +1,30 @@
+use core::fmt;
+use std::error;
 use std::fs;
 use std::io;
 use std::io::{BufRead, BufReader, Write};
 
-use super::Result;
+use super::Result as BoxResult;
 
-pub fn open(filename: String) -> Result<()> {
+#[derive(Debug)]
+struct InterpreterError;
+
+impl error::Error for InterpreterError {}
+
+impl fmt::Display for InterpreterError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "failed to interpret")
+    }
+}
+
+pub fn open(filename: String) -> BoxResult<()> {
     let source = fs::read_to_string(filename)?;
     interpret(source)?;
 
     Ok(())
 }
 
-pub fn interactive() -> Result<()> {
+pub fn interactive() -> BoxResult<()> {
     let mut reader = BufReader::new(io::stdin());
 
     loop {
@@ -26,14 +39,17 @@ pub fn interactive() -> Result<()> {
             println!();
             break;
         } else {
-            interpret(line)?;
+            match interpret(line) {
+                Ok(()) => continue,
+                Err(e) => eprintln!("response: {}", e),
+            }
         }
     }
 
     Ok(())
 }
 
-fn interpret(source: String) -> Result<()> {
+fn interpret(source: String) -> Result<(), InterpreterError> {
     if !source.trim().is_empty() {
         println!("interpreting... \"{}\"", source.trim());
     }
